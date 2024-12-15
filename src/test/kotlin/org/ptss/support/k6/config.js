f063b1a3d500a7bfddc632e9c525af589afafda2
RamonKea-inh <687187@student.inholland.nl>
2024-12-15 18:03:12 +0100

    added a package for k6 and  made the ROUTE_HOST available for other steps

--- src/test/kotlin/org/ptss/support/k6/config.js
+++ src/test/kotlin/org/ptss/support/k6/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://${process.env.ROUTE_HOST}';
+export const API_URL = 'http://${process.env.ROUTE_HOST}';
 export const PAYLOAD = JSON.stringify({ key: 'value' });
 export const HEADERS = { 'Content-Type': 'application/json' };
