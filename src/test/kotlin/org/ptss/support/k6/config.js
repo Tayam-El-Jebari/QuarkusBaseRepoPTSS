cd3fa20274d9dcb9d448969c87530389da695ee8
RamonKea-inh <687187@student.inholland.nl>
2024-12-15 17:40:26 +0100

    Changed the API_URL

--- src/test/kotlin/org/ptss/support/k6/config.js
+++ src/test/kotlin/org/ptss/support/k6/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://${process.env.ROUTE_HOST}';
 export const PAYLOAD = JSON.stringify({ key: 'value' });
 export const HEADERS = { 'Content-Type': 'application/json' };
