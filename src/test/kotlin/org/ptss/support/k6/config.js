22906d88c3ac908429d500269bae4e856b1a159e
RamonKea-inh <687187@student.inholland.nl>
2024-12-16 01:35:47 +0100

    Update config.js

--- src/test/kotlin/org/ptss/support/k6/config.js
+++ src/test/kotlin/org/ptss/support/k6/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://${__ENV.ROUTE_HOST}';
+export const API_URL = `https://${__ENV.ROUTE_HOST}`;
 export const PAYLOAD = JSON.stringify({ key: 'value' });
 export const HEADERS = { 'Content-Type': 'application/json' };
