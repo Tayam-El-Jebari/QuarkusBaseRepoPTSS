528beeed75fe0b4d20736774807c0352ca91ddd6
SemPlaatsman <semplaatsman@gmail.com>
2025-01-05 02:07:40 +0100

    Added user context to authentication

--- src/main/kotlin/org/ptss/support/security/TokenUserExtractor.kt
+++ src/main/kotlin/org/ptss/support/security/TokenUserExtractor.kt
@@ -5,31 +5,37 @@ import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import org.eclipse.microprofile.jwt.JsonWebToken
 import org.ptss.support.domain.enums.Role
+import org.ptss.support.security.context.UserContext
+import java.time.Instant
+import java.util.UUID
 
 @ApplicationScoped
 class TokenUserExtractor @Inject constructor(
     private val jwtParser: JWTParser
 ) {
-    fun getUserId(token: String): String? {
+    fun extractUserContext(token: String): Result<UserContext> {
         return runCatching {
             val jwt = jwtParser.parse(token)
-            if (isExpired(jwt)) return null
-            jwt.getClaim<String>("user_id")
-        }.getOrNull()
+            if (isExpired(jwt)) {
+                return Result.failure(Exception("Token expired"))
+            }
+
+            val userId = jwt.getClaim<String>("user_id")?.let { runCatching { UUID.fromString(it) }.getOrNull() }
+                ?: return Result.failure(Exception("Invalid or missing user_id in token"))
+
+            val groupId = jwt.getClaim<String>("group_id")?.let { runCatching { UUID.fromString(it) }.getOrNull() }
+
+            UserContext(
+                userId = userId,
+                groupId = groupId,
+                roles = jwt.groups.mapNotNull { runCatching { Role.fromString(it) }.getOrNull() }.toSet(),
+                hasPin = jwt.getClaim("has_pin") ?: false
+            )
+        }
     }
 
-    fun getUserRoles(token: String): Set<Role> {
-        return runCatching {
-            val jwt = jwtParser.parse(token)
-            if (isExpired(jwt)) return emptySet()
-            jwt.groups.mapNotNull {
-                runCatching { Role.fromString(it) }.getOrNull()
-            }.toSet()
-        }.getOrElse { emptySet() }
-    }
     // basic safety measure for expired tokens when service-to-service calls using the same token expires midway through
     private fun isExpired(jwt: JsonWebToken): Boolean {
-        val now = System.currentTimeMillis() / 1000
-        return jwt.expirationTime < now
+        return jwt.expirationTime < Instant.now().epochSecond
     }
 }
